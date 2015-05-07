118c635d81d3c8a6260018f96c203c2cea6ae4be
James Pike <jpike@chilon.net>
2015-05-07 13:26:12 +0100

    Initial commit, $q.allSettled and $q.map + tests.

---
+++ test/tests.spec.js
@@ -0,0 +1,97 @@
+'use strict';
+
+describe('angular-promise-extras', function () {
+  beforeEach(module('ngPromiseExtras'))
+
+  var $q, $rootScope
+
+  beforeEach(inject(function(_$q_, _$rootScope_) {
+    $q = _$q_
+    $rootScope = _$rootScope_
+  }))
+
+  describe('$q.allSettled', function() {
+    var deferreds, proms
+
+    beforeEach(function() {
+      deferreds = [ $q.defer(), $q.defer(), $q.defer() ]
+      proms = deferreds.map(function(deferred) {
+        return deferred.promise
+      })
+    })
+
+    it('works when passed an array', function(done) {
+      var testProm = $q.allSettled(proms).then(function(results) {
+        expect(results[0].state).toBe('fulfilled')
+        expect(results[1].state).toBe('rejected')
+        expect(results[2].state).toBe('fulfilled')
+        expect(results[0].value).toBe(1)
+        expect(results[1].reason).toBe(2)
+        expect(results[2].value).toBe(3)
+      })
+      .then(done, done.fail)
+
+      deferreds[0].resolve(1)
+      deferreds[1].reject(2)
+      deferreds[2].resolve(3)
+      $rootScope.$apply()
+
+      return testProm
+    })
+
+    it('works when passed a hash', function(done) {
+      var promsObj = {
+        a: proms[0],
+        b: proms[1],
+        c: proms[2]
+      }
+
+      var testProm = $q.allSettled(promsObj).then(function(results) {
+        expect(results.a.state).toBe('fulfilled')
+        expect(results.b.state).toBe('rejected')
+        expect(results.c.state).toBe('fulfilled')
+        expect(results.a.value).toBe(1)
+        expect(results.b.reason).toBe(2)
+        expect(results.c.value).toBe(3)
+      })
+      .then(done, done.fail)
+
+      deferreds[0].resolve(1)
+      deferreds[1].reject(2)
+      deferreds[2].resolve(3)
+
+      $rootScope.$apply()
+      return testProm
+    })
+  })
+
+  describe('$q.map', function() {
+    it('works when passed an array', function(done) {
+      var values = [1, 2, 3]
+      var testProm = $q.map(values, function(val, idx) {
+        return $q(function(resolve) { resolve(val * idx) })
+      })
+      .then(function(vals) {
+        expect(vals).toEqual([0, 2, 6])
+      })
+      .then(done, done.fail)
+
+      $rootScope.$apply()
+      return testProm
+    })
+
+    it('works when passed an object', function(done) {
+      var values = { a: 1, b: 2, c: 3 }
+      var testProm = $q.map(values, function(val, key) {
+        return $q(function(resolve) { resolve(key + ':' + val) })
+      })
+      .then(function(vals) {
+        expect(vals).toEqual({ a: 'a:1', b: 'b:2', c: 'c:3' })
+      })
+      .then(done, done.fail)
+
+      $rootScope.$apply()
+      return testProm
+    })
+  })
+})
